b00a7f93b9ccecbe12eddf6b69e9eba0f1cc613c
pah69 <engineerpah@gmail.com>
2025-02-13 14:38:09 +0700

    latest

---
+++ Bus Protocol/AHB/ahb_slave.sv
@@ -0,0 +1,17 @@
+module ahb_slave(
+    input logic hclk,
+    input logic hresetn,
+
+    input logic [11:2] haddr, // system address bus
+    input logic hready,
+    input logic hsel,
+    input logic [2:0] hsize,
+    input logic htrans,
+    input logic [31:0] hwdata,
+    input logic hwrite,
+
+    output logic [31:0] hrdata,
+    output logic hready_o;
+    output logic [1:0] hresp,
+
+)
